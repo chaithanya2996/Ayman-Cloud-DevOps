d78f0c9149427daf25bf3d5e6d825847b57f715a
Chaithanya G <chaithanya2996@gmail.com>
2026-08-11 10:55:59 +0530

    updated

--- DevOps/IAAC/virutalmachine/provider.tf
+++ DevOps/IAAC/virutalmachine/provider.tf
@@ -9,5 +9,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  
+  # client_id = "31626c5f-43ee-42ad-bce7-610b54e1ad36"
+  # client_secret = ""
+  # tenant_id = "1ee398d7-c819-4915-a6dc-29750dcc661f"
+  # subscription_id = "f69fb365-d758-4000-8f93-1d76a718cfb6"
 }
